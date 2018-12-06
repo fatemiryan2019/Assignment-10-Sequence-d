f990ab28ccab7abbf7120d2811e8ec68675868e7
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2018-12-05 20:02:29 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,67 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        First 10 Square Numbers: 1 4 9 16 25 36 49 64 81 100
+//        Calculate the numbers using while or do...while loops to create the numbers
+        System.out.print("\nThe First 10 Square Numbers: ");
+        int n = 0;
+        while (n < 10) {
+            int sqr = n * n;
+            n++;
+            System.out.printf("%d ", sqr);
+        }
+
+//        First 10 Cube Numbers: 1 8 27 64 125 216 343 512 729 1000
+//        Calculate the numbers using while or do...while loops to create the numbers
+        System.out.print("\nThe First 10 Cube Numbers: ");
+        n = 0;
+        while (n < 10) {
+            int cube = n * n * n;
+            System.out.printf("%d ", cube);
+            n++;
+        }
+
+//        First 10 Fibonacci Numbers from 1: 1 2 3 5 8 13 21 34 55 89
+//        Calculate the numbers using while or do...while loops to create the numbers
+        System.out.print("\nThe First 10 Fibonacci Numbers from 1: ");
+        int[]fib = new int[10];
+        fib[0] = 1;
+        fib[1] = 2;
+        for(int i = 2; i < fib.length; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        for(int i = 0; i < fib.length; i++) {
+            System.out.printf("%d ", fib[i]);
+            n = 1;
+        }
+
+//        First 10 Prime Numbers: 2 3 5 7 11 13 17 19 23 29
+//        Calculate the numbers using while or do...while loops to create the numbers
+        System.out.print("\nThe First 10 Prime Numbers: ");
+        for (int num = 2; num < 30; num++) {
+            boolean flag = false;
+            for (int i = 2; i <= num / 2; ++i) {
+                if (num % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+                System.out.printf("%d ", num);
+        }
+
+//        First 10 Triangle Numbers: 1 3 6 10 15 21 28 36 45 55
+//        Calculate the numbers using while or do...while loops to create the numbers
+        System.out.print("\nThe First 10 Triangle Numbers: ");
+        int[]tri = new int[10];
+        tri[0] = 1;
+        for(int i = 1; i < tri.length; i++) {
+            tri[i] = tri[i - 1] + i + 1;
+        }
+        for(int i = 0; i < tri.length; i++) {
+            System.out.printf("%d ", tri[i]);
+        }
+
+    }
+}
