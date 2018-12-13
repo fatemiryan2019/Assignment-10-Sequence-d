a80a4c8500281cfae28ac8f1de76d132e1cca7e8
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2018-12-12 22:13:37 -0800

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ public class Main {
 
 //        First 10 Square Numbers: 1 4 9 16 25 36 49 64 81 100
 //        Calculate the numbers using while or do...while loops to create the numbers
+
         System.out.print("\nThe First 10 Square Numbers: ");
         int n = 0;
         while (n < 10) {
@@ -14,6 +15,7 @@ public class Main {
 
 //        First 10 Cube Numbers: 1 8 27 64 125 216 343 512 729 1000
 //        Calculate the numbers using while or do...while loops to create the numbers
+
         System.out.print("\nThe First 10 Cube Numbers: ");
         n = 0;
         while (n < 10) {
@@ -24,44 +26,54 @@ public class Main {
 
 //        First 10 Fibonacci Numbers from 1: 1 2 3 5 8 13 21 34 55 89
 //        Calculate the numbers using while or do...while loops to create the numbers
+
         System.out.print("\nThe First 10 Fibonacci Numbers from 1: ");
-        int[]fib = new int[10];
-        fib[0] = 1;
-        fib[1] = 2;
-        for(int i = 2; i < fib.length; i++) {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-        for(int i = 0; i < fib.length; i++) {
-            System.out.printf("%d ", fib[i]);
-            n = 1;
-        }
+        int y = 0;
+        int z = 0;
+        int fibonacci = 1;
+        do {
+            y = z;
+            z = fibonacci;
+            fibonacci = y + z;
+            System.out.printf("%d ", fibonacci);
+        } while (fibonacci <= 55);
+        System.out.println();
 
 //        First 10 Prime Numbers: 2 3 5 7 11 13 17 19 23 29
 //        Calculate the numbers using while or do...while loops to create the numbers
-        System.out.print("\nThe First 10 Prime Numbers: ");
-        for (int pri = 2; pri < 30; pri++) {
-            boolean prime = false;
-            for (int i = 2; i <= pri / 2; ++i) {
-                if (pri % i == 0) {
-                    prime = true;
-                    break;
+
+        System.out.print("The First 10 Prime Numbers are: ");
+        while(n<=10){
+            int num = 1;
+            while (num<=29) {
+                num++;
+                boolean xprime = false;
+                int x = 1;
+                while (x<=num/3) {
+                    x++;
+                    if (num%x==0) {
+                        xprime = true;
+                        break;
+                    }
+                }
+                if (!xprime) {
+                    System.out.print(num + " ");
                 }
             }
-            if (!prime)
-                System.out.printf("%d ", pri);
+            n++;
         }
+        System.out.println();
+
 
 //        First 10 Triangle Numbers: 1 3 6 10 15 21 28 36 45 55
 //        Calculate the numbers using while or do...while loops to create the numbers
-        System.out.print("\nThe First 10 Triangle Numbers: ");
-        int[]tri = new int[10];
-        tri[0] = 1;
-        for(int i = 1; i < tri.length; i++) {
-            tri[i] = tri[i - 1] + i + 1;
-        }
-        for(int i = 0; i < tri.length; i++) {
-            System.out.printf("%d ", tri[i]);
-        }
+
+        System.out.print("The First 10 Triangle Numbers: ");
+        n = 1;
+        while (n <= 10) {
+            int triangle = (n * (n + 1) / 2);
+            System.out.printf("%d ", triangle);
+            n++; }
 
     }
 }
